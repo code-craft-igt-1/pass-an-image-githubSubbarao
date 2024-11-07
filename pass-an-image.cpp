7758af983a8569faddcc54db61f7e899a695e023
githubSubbarao <101566444+githubSubbarao@users.noreply.github.com>
2024-11-07 15:24:41 +0530

    Update pass-an-image.cpp

--- pass-an-image.cpp
+++ pass-an-image.cpp
@@ -2,11 +2,11 @@
 #include "brightener.h"
 
 int main() {
-    Image image;
-    image.rows = 512;
-    image.columns = 512;
+    auto image = std::make_unique<Image>();
+    image->rows = 512;
+    image->columns = 512;
 	std::cout << "Brightening a 512 x 512 image\n";
-    ImageBrightener brightener(image);
-    int attenuatedCount = brightener.BrightenWholeImage();
+    auto brightener = std::make_unique<ImageBrightener>(*image);
+    int attenuatedCount = brightener->BrightenWholeImage();
     std::cout << "Attenuated " << attenuatedCount << " pixels\n";
 }
